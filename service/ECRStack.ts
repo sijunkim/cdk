e12a8235fd6e52288524ff06b994dc3e4243d7f4
sijunkim <papaya9349@naver.com>
2023-03-13 21:29:38 +0900

    move stacks

---
+++ service/ECRStack.ts
@@ -0,0 +1,14 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as env from '../utilities/env';
+
+export class ECRStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // ECR
+    const repositoryProps: cdk.aws_ecr.RepositoryProps = { repositoryName: env.CDK_REPOSITORY };
+    const erc = new ecr.Repository(this, env.CDK_REPOSITORY, repositoryProps);
+  }
+}
