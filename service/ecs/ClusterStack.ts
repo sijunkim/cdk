6ffbf0e7fbe94496a62b5aedef16a4992d746d1a
sijunkim <papaya9349@naver.com>
2023-03-16 22:56:01 +0900

    stack 파일 정리

---
+++ service/ecs/ClusterStack.ts
@@ -0,0 +1,19 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as env from '../../utilities/env';
+
+export class ClusterStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // VPC
+    const vpcLookupOptions: cdk.aws_ec2.VpcLookupOptions = { vpcName: env.CDK_VPC };
+    const vpc = ec2.Vpc.fromLookup(this, env.CDK_VPC, vpcLookupOptions);
+
+    // CLUSTER
+    const cluster = new ecs.Cluster(this, env.CDK_CLUSTER, { vpc });
+  }
+}
--- stack/albPublicSubnetStack.ts
+++ stack/albPublicSubnetStack.ts
@@ -3,20 +3,73 @@ import * as env from '../utilities/env';
 import * as cdk from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
-import { vpcModule, ecrModule } from '../service/serviceModule';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as secretsManager from 'aws-cdk-lib/aws-secretsmanager';
+import * as serviceModule from '../service/serviceModule';
 
 export class ALBPublicSubnetStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // VPC, Public Subnet 2개
-    const publicSubnetConfig1 = vpcModule.getPublicSubnet(env.CDK_PUBLIC_SUBNET1);
-    const publicSubnetConfig2 = vpcModule.getPublicSubnet(env.CDK_PUBLIC_SUBNET2);
-    const vpcProps = vpcModule.getVPC(env.CDK_VPC, [publicSubnetConfig1, publicSubnetConfig2]);
+    const publicSubnetConfig1 = serviceModule.vpc.getPublicSubnet(env.CDK_PUBLIC_SUBNET1);
+    const publicSubnetConfig2 = serviceModule.vpc.getPublicSubnet(env.CDK_PUBLIC_SUBNET2);
+    const vpcProps = serviceModule.vpc.getVPC(env.CDK_VPC, [publicSubnetConfig1, publicSubnetConfig2]);
     const vpc = new ec2.Vpc(this, id, vpcProps);
 
     // ECR
-    const repositoryProps = ecrModule.getRepository(env.CDK_REPOSITORY);
-    const erc = new ecr.Repository(this, id, repositoryProps);
+    const repositoryProps = serviceModule.ecr.getRepository(env.CDK_REPOSITORY);
+    const repository = new ecr.Repository(this, id, repositoryProps);
+
+    // CLUSTER
+    const cluster = new ecs.Cluster(this, id, { vpc });
+
+    // Task Definition
+    const taskDefinitionProps: ecs.TaskDefinitionProps = {
+      family: env.CDK_TASK_DEFINITION,
+      memoryMiB: '2048',
+      cpu: '1024',
+      compatibility: ecs.Compatibility.FARGATE,
+      taskRole: iam.Role.fromRoleArn(this, id, env.CDK_ECS_TASK_EXECUTION_ROLE_ARN),
+      executionRole: iam.Role.fromRoleArn(this, id, env.CDK_ECS_TASK_EXECUTION_ROLE_ARN),
+    };
+    // serviceModule.taskDefinition.getTaskDefinition(this, id, env.CDK_TASK_DEFINITION);
+    const taskDefinition = new ecs.TaskDefinition(this, id, taskDefinitionProps);
+    const containerDefinition = taskDefinition.addContainer(env.CDK_CONTAINER, {
+      image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
+      cpu: 1,
+      memoryLimitMiB: 2048,
+      // secrets manager 연동
+      // secrets: {},
+      // OpenSearch 연동
+      // logging: ecs.LogDrivers.firelens({
+      //   options: {
+
+      //   }
+      // },
+    });
+    containerDefinition.addPortMappings({
+      containerPort: 8801,
+      hostPort: 80,
+      name: env.CDK_CONTAINER,
+      protocol: ecs.Protocol.TCP,
+      appProtocol: ecs.AppProtocol.http,
+    });
+    // const containerDefinitionOptions: ecs.ContainerDefinitionOptions = {
+    //   image: ecs.ContainerImage.fromEcrRepository(repository),
+    //   cpu: 1,
+    //   memoryReservationMiB: 2048,
+
+    // };
+
+    // Secrets Manager
+    // arn:aws:secretsmanager:<region>:<aws_account_id>:secret:<secret_name>
+    // arn:aws:secretsmanager:ap-northeast-2:145767916253:secret:ADMIN_SECRETS-rSwZrk
+    // arn:aws:secretsmanager:ap-northeast-2:145767916253:secret:ADMIN_SECRETS-rSwZrk:ETHERSCAN_API_KEY::
+    // const secretCompleteArn = 'arn:aws:secretsmanager:ap-northeast-2:{aws accounts id}:secret:BATCH_SECRETS-6b9Bhb';
+    // const mySecretFromCompleteArn = ecs.Secret.fromSecretsManager(secretCompleteArn, 'SecretFromCompleteArn');
+
+    // ECS
   }
 }
