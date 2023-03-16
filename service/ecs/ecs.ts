a910e62b088090ed7a109b05393d1af43d5e82e1
sijunkim <papaya9349@naver.com>
2023-03-16 23:22:33 +0900

    ecs 모듈 추가

---
+++ service/ecs/ecs.ts
@@ -0,0 +1,36 @@
+import * as cdk from 'aws-cdk-lib';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as iam from 'aws-cdk-lib/aws-iam';
+
+export function getTaskDefinition(family: string, taskRole: iam.IRole, executionRole: iam.IRole): ecs.TaskDefinitionProps {
+  return {
+    family,
+    memoryMiB: '2048',
+    cpu: '1024',
+    compatibility: ecs.Compatibility.FARGATE,
+    taskRole,
+    executionRole,
+  };
+}
+
+export function getContainerDefinition(repository: ecr.IRepository): ecs.ContainerDefinitionOptions {
+  return {
+    image: ecs.ContainerImage.fromEcrRepository(repository),
+    // secrets manager 연동
+    // secrets: {},
+    // OpenSearch 연동
+    // logging: ecs.LogDrivers.firelens({
+    //   options: {
+
+    //   }
+    // },
+  };
+}
+
+// Secrets Manager
+// arn:aws:secretsmanager:<region>:<aws_account_id>:secret:<secret_name>
+// arn:aws:secretsmanager:ap-northeast-2:145767916253:secret:ADMIN_SECRETS-rSwZrk
+// arn:aws:secretsmanager:ap-northeast-2:145767916253:secret:ADMIN_SECRETS-rSwZrk:ETHERSCAN_API_KEY::
+// const secretCompleteArn = 'arn:aws:secretsmanager:ap-northeast-2:{aws accounts id}:secret:BATCH_SECRETS-6b9Bhb';
+// const mySecretFromCompleteArn = ecs.Secret.fromSecretsManager(secretCompleteArn, 'SecretFromCompleteArn');
